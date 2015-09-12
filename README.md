f71e464309f70a52883b15d93acba68c9e911b64
ksrt12 <kazakov12stepan2012@live.ru>
2015-09-12 15:39:37 +0600

    update

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # android_device_huawei_u8950
-# android_device_huawei_u8950
+
+SlimLP device tree for Huawei U8950 (Honor Pro)
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,1 +1,6 @@
-add_lunch_combo slim_u8950-userdebug
+if [ ! -e device/huawei/u8950/ok ]
+then
+sh device/huawei/msm7x27a-common/patches/apply.sh
+sh device/huawei/u8950/patches/apply.sh
+touch device/huawei/u8950/ok
+fi
