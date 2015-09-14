6bda4ae0bf686298d127ec5f37852c8b3b10547d
Kazakov Stepan <kazakov12stepan2012@live.ru>
2015-09-14 20:17:21 +0500

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,6 +1,6 @@
-if [ ! -e device/huawei/u8950/ok ]
+if [ ! -e device/huawei/ok ]
 then
 sh device/huawei/msm7x27a-common/patches/apply.sh
 sh device/huawei/u8950/patches/apply.sh
-touch device/huawei/u8950/ok
+touch device/huawei/ok
 fi
