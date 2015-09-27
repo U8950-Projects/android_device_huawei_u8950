ee74ed8685b30783369a303a36dbb59f4a022915
ksrt12 <kazakov12stepan2012@live.ru>
2015-09-27 15:53:38 +0600

    15532709

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -48,18 +48,17 @@ BOARD_CUSTOM_GRAPHICS := ../../../device/huawei/u8950/recovery/graphics.c
 TARGET_RECOVERY_FSTAB := device/huawei/u8950/rootdir/fstab.huawei
 
 # Wi-Fi
-WPA_SUPPLICANT_VERSION      := VER_0_8_X
-BOARD_WLAN_DEVICE           := bcmdhd
+WPA_SUPPLICANT_VERSION := VER_0_8_X
+BOARD_WLAN_DEVICE := bcmdhd
 BOARD_WPA_SUPPLICANT_DRIVER := NL80211
 BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
-BOARD_HOSTAPD_DRIVER        := NL80211
-BOARD_HOSTAPD_PRIVATE_LIB   := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
+BOARD_HOSTAPD_DRIVER := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
 WIFI_DRIVER_FW_PATH_AP := "/system/etc/fw_4330_b2.bin"
 WIFI_DRIVER_FW_PATH_STA := "/system/etc/fw_4330_b2.bin"
 WIFI_DRIVER_FW_PATH_P2P := "/system/etc/fw_4330_b2.bin"
-#WIFI_DRIVER_FW_PATH_AP := "/system/vendor/firmware/fw_bcmdhd_apsta.bin"
-#WIFI_DRIVER_FW_PATH_STA := "/system/vendor/firmware/fw_bcmdhd.bin"
 WIFI_DRIVER_FW_PATH_PARAM := "/sys/module/dhd/parameters/firmware_path"
+TARGET_CUSTOM_WIFI := ../../device/huawei/msm7x27a-common/libhardware_legacy/wifi/wifi.c
 
 WIFI_DRIVER_MODULE_ARG := "firmware_path=/system/etc/fw_4330_b2.bin nvram_path=/system/etc/nvram_4330.txt"
 WIFI_DRIVER_MODULE_NAME := "dhd"
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,3 +4,4 @@ sh device/huawei/msm7x27a-common/patches/apply.sh
 sh device/huawei/u8950/patches/apply.sh
 touch device/huawei/ok
 fi
+add_lunch_combo slim_u8950-userdebug
