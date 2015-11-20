8cb803d4a96fdf575a37e2d27497befdce7c25ed
ksrt12 <kazakov12stepan2012@live.ru>
2015-11-20 23:44:56 +0600

    fix system/core

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,6 +1,7 @@
 # Patches
 if [ ! -e device/huawei/ok ]
 then
+cd system/core; git checkout 08552a83a528eb0c28c77ca39d6853c6d634c729; cd -
 sh device/huawei/msm7x27a-common/patches/apply.sh
 sh device/huawei/u8950/patches/apply.sh
 touch device/huawei/ok
