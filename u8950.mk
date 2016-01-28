91523bcea4e230f1667eaba2585ebe1b45745993
alexey podvoyskiy <fell978@yandex.ru>
2016-01-28 10:20:51 +0300

    Update u8950.mk

--- u8950.mk
+++ u8950.mk
@@ -103,4 +103,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full.mk)
 
 $(call inherit-product, frameworks/native/build/phone-hdpi-512-dalvik-heap.mk)
 
-$(call inherit-product-if-exists, vendor/huawei/u8950/u8950-vendor.mk)
+$(call inherit-product, hardware/broadcom/wlan/bcmdhd/firmware/bcm4330/device-bcm.mk)
+
+$(call inherit-product, vendor/huawei/u8950/u8950-vendor.mk)
